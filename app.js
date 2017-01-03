c53eedd8aaf0835dabdd80170a92751a5698bedb
mariajcb <bogomazm@gmail.com>
2017-01-03 16:11:16 -0700

    required doenv in app.js

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 'use strict';
 
+if(process.env.NODE_ENV != 'production') {
+	require('dotenv').config()
+}
+
 const express = require('express');
 const crypto = require('crypto');
 const bodyParser = require('body-parser');
